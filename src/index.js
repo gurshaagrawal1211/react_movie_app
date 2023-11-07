064f9c8cab6adede52c26d16a45ab7d14a39fdb7
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-11-07 23:16:50 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
+// 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
